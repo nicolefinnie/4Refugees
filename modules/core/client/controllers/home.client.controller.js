b6a22ab557ce05b926b138e13c5fa237593782eb
Nicole Finnie <Nicole.Finnie@de.ibm.com>
2015-11-05 14:30:22 +0100

    fixed indentation using eclipse formatter with customized tab

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,29 +4,24 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
+    
+    // Hide or show main buttons findHelp/helpOthers or hidden sub-buttons
+    $scope.toggleFindHelp = function() {
+      $scope.mainCallToButtons = !$scope.mainCallToButtons;
+      $scope.findHelpCallToButtons = !$scope.findHelpCallToButtons;
+    };
+    $scope.toggleHelpOthers = function() {
+      $scope.mainCallToButtons = !$scope.mainCallToButtons;
+      $scope.helpOthersCallToButtons = !$scope.helpOthersCallToButtons;
+    };
   }
 ]);
 
-angular.module('core').controller('HomeController', ['$scope', 
-   function($scope) {
-
-	$scope.toggleFindHelp = function() {
-		$scope.mainCallToButtons = !$scope.mainCallToButtons;
-		$scope.findHelpCallToButtons = !$scope.findHelpCallToButtons;
-    };
-    
-    $scope.toggleHelpOthers = function() {
-		$scope.mainCallToButtons = !$scope.mainCallToButtons;
-		$scope.helpOthersCallToButtons = !$scope.helpOthersCallToButtons;
-    };
- }
-]);
-
 angular.module('core').controller('CarouselControl', function ($scope) {
-  //$scope.myInterval = 5000;
-  //$scope.noWrapSlides = false;
+//$scope.myInterval = 5000;
+//$scope.noWrapSlides = false;
   var slides = $scope.slides = [];
-
+  
   $scope.addSlide = function(index) {
     var newWidth = 600 + slides.length + 1;
     slides.push({
