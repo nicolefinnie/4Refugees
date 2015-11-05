8588dd3e27fdc938e2f070ee5b827cb75d1b9025
Markus Mueller <sedgewickmm18@googlemail.com>
2015-11-05 14:51:27 +0000

    Replaced old images with free to use for commercial purposes images mostly taken from Wikimedia modified controller to take more than 9 images

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -4,34 +4,48 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
   function ($scope, Authentication) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-    
-    // Hide or show main buttons findHelp/helpOthers or hidden sub-buttons
-    $scope.toggleFindHelp = function() {
-      $scope.mainCallToButtons = !$scope.mainCallToButtons;
-      $scope.findHelpCallToButtons = !$scope.findHelpCallToButtons;
-    };
-    $scope.toggleHelpOthers = function() {
-      $scope.mainCallToButtons = !$scope.mainCallToButtons;
-      $scope.helpOthersCallToButtons = !$scope.helpOthersCallToButtons;
-    };
   }
+]);
+
+angular.module('core').controller('HomeController', ['$scope', 
+   function($scope) {
+
+     $scope.toggleFindHelp = function() {
+       $scope.mainCallToButtons = !$scope.mainCallToButtons;
+       $scope.findHelpCallToButtons = !$scope.findHelpCallToButtons;
+     };
+    
+     $scope.toggleHelpOthers = function() {
+       $scope.mainCallToButtons = !$scope.mainCallToButtons;
+       $scope.helpOthersCallToButtons = !$scope.helpOthersCallToButtons;
+     };
+   }
 ]);
 
 angular.module('core').controller('CarouselControl', function ($scope) {
 //$scope.myInterval = 5000;
 //$scope.noWrapSlides = false;
   var slides = $scope.slides = [];
-  
-  $scope.addSlide = function(index) {
+
+  $scope.addSlide = function(index1,index2) {
+    var index = index1;
     var newWidth = 600 + slides.length + 1;
+    var imgName = 'modules/core/client/img/startpage/img';
+    if (index2 !== 0) {
+      imgName = 'modules/core/client/img/startpage/imh';
+      index = index2;
+    }
+
     slides.push({
-      //image: '//placekitten.com/' + newWidth + '/300',
-      image: 'modules/core/client/img/startpage/img' + index + '.jpg',
+      image: imgName + index + '.jpg',
       text: ['Refugees', 'Refugees', 'Future citizens'][slides.length % 3]
     });
   };
-  for (var i=0; i<8; i++) {
-    $scope.addSlide(i+1);
+  for (var i=0; i<9; i++) {
+    $scope.addSlide(i+1,0);
+  }
+  for (i=0; i<2; i++) {
+    $scope.addSlide(i+1,i+1);
   }
 });
 
