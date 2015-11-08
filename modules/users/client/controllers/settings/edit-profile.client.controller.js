924f2044e4a5712d4a85541075c8db3bebfabd5d
kaliDETW <it.kai.liu@gmail.com>
2015-11-08 01:43:30 +0100

    first version of tags in profile page with debug information
    only UI component, respective APi is still missing and user object must
    still be enhanced for new fields.

--- modules/users/client/controllers/settings/edit-profile.client.controller.js
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -1,28 +1,74 @@
 'use strict';
 
-angular.module('users').controller('EditProfileController', ['$scope', '$http', '$location', 'Users', 'Authentication',
-  function ($scope, $http, $location, Users, Authentication) {
-    $scope.user = Authentication.user;
+angular.module('users').controller('EditProfileController', ['$scope', '$http', '$location', 'Users', 'Authentication', '$log',
+  function (ctrl, $http, $location, Users, Authentication, log) {
+    ctrl.user = Authentication.user;
+    
+    log.info('The user object from Authentication.user:');
+    log.info(ctrl.user);
 
     // Update a user profile
-    $scope.updateUserProfile = function (isValid) {
-      $scope.success = $scope.error = null;
-
+    ctrl.updateUserProfile = function (isValid) {
+      ctrl.success = ctrl.error = null;
+      
       if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'userForm');
+        ctrl.$broadcast('show-errors-check-validity', 'userForm');
 
         return false;
       }
 
-      var user = new Users($scope.user);
+      var user = new Users(ctrl.user);
 
       user.$update(function (response) {
-        $scope.$broadcast('show-errors-reset', 'userForm');
+        ctrl.$broadcast('show-errors-reset', 'userForm');
 
-        $scope.success = true;
+        ctrl.success = true;
         Authentication.user = response;
       }, function (response) {
-        $scope.error = response.data.message;
+        ctrl.error = response.data.message;
+      });
+    };
+
+    // TODO load available tags here, already set ones should come from user object authentication.user later;
+    // TODO provide API to store and retrieve tags
+    // TODO extend user object with tags and aboutMe section
+    // TODO extend API to safe those new fields
+    // TODO custom template to format the tags, e.g have those tags next to each other
+    ctrl.user.tags = [{ tagName: '\"samurai on the toilet\" - by takeshi kitano (1970)' },
+                      { tagName: '(>_<)' },
+                      { tagName: '(o_o)' },
+                      { tagName: '(O_O)' },
+                      { tagName: '(^_^)' }
+                     ];
+
+    // mocked data until I figured out how to create APIs
+    ctrl.loadTags = function($query) {
+      return $http.get('/public/lib/tags.json', { cache: true }).then(function(response) {
+//          var tags = response.data;
+        log.info('DEBUG - resonse.data content: ' + response.data);
+        var tags = [
+                    { tagName: 'sleepy' },
+                    { tagName: 'super sleepy' },
+                    { tagName: 'Translation' },
+                    { tagName: 'Offices' },
+                    { tagName: 'Politics' },
+                    { tagName: 'Toys' },
+                    { tagName: 'Food' },
+                    { tagName: 'Calculation' },
+                    { tagName: 'Stuff' },
+                    { tagName: 'Cheese' },
+                    { tagName: 'Sports' },
+                    { tagName: 'Smelly feet' },
+                    { tagName: 'Chocolate' },
+                    { tagName: 'Fun' },
+                    { tagName: 'Sleep' },
+                    { tagName: 'SomeSleep1' },
+                    { tagName: 'SleepDeprivedProgrammer' },
+                    { tagName: 'PowerSleep' }
+        ];
+        return tags.filter(function(tag) {
+          return tag.tagName.toLowerCase().indexOf($query.toLowerCase()) !== -1;
+        });
       });
     };
   }
