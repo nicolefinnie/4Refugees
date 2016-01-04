1a6643464e3787b2a32054da4633a2eaa468b7a4
Nicole Finnie <Nicole.Finnie@de.ibm.com>
2016-01-04 15:37:54 +0100

    task 5925 fixed the edit-profile css style and merged Kai's fix for tags

--- modules/users/client/services/tags.client.service.js
+++ modules/users/client/services/tags.client.service.js
@@ -1,16 +1,21 @@
 'use strict';
 
 //Tags service used for communicating with the tags REST endpoint
+//REST endpoint to retrieve available tags from DB
 angular.module('core').factory('Tags', ['$resource',
   function ($resource) {
     return $resource('api/tags', {
       method: 'GET'
     });
   }
+]);
 
+// REST endpoint to store tags
+angular.module('core').factory('Tags', ['$resource',
   function ($resource) {
     return $resource('api/tags/save', {
       method: 'PUT'
     });
   }
 ]);
+ 
