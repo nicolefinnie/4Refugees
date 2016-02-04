9c1d15b9a1e00b057da54d3b4cc5ca8d6fecd777
KALI <KALI@de.ibm.com>
2016-02-04 15:14:06 +0100

    Task 6926: change UI language automatically after logging in - use the changeLanguage service after logging in

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -64,19 +64,16 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$root
 
     $scope.signin = function (isValid) {
       $scope.error = null;
-
       if (!isValid) {
         $scope.$broadcast('show-errors-check-validity', 'userForm');
-
         return false;
       }
-
       $http.post('/api/auth/signin', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
-
         // And redirect to the previous or home page
         $state.go($state.previous.state.name || 'home', $state.previous.params);
+        LanguageService.changeLanguage($scope, $scope.authentication.user.languagePreference, false);
       }).error(function (response) {
         $scope.error = response.message;
       });
