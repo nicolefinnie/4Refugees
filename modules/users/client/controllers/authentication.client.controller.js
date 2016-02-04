8294ac97fe8023d79bcfaec4d0bcd54e29f99d17
KALI <KALI@de.ibm.com>
2016-02-04 13:57:46 +0100

    Task 6927: save the current selected global language setting during registration process automatically (its magic!)

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -3,7 +3,7 @@
 angular.module('users').controller('AuthenticationController', ['$scope', '$rootScope', '$state', '$http', '$location', '$window', 
                                                                 'Authentication', 'PasswordValidator', 'LanguageService',
   function ($scope, $rootScope, $state, $http, $location, $window, Authentication, PasswordValidator, LanguageService) {
-  
+
     // language change clicked
     $rootScope.$on('tellAllControllersToChangeLanguage', function(){
       //refresh view properties of home
@@ -30,17 +30,14 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$root
     $rootScope.hideFooter = true;
     // Get an eventual error defined in the URL query string:
     $scope.error = $location.search().err;
-
     // if advanced sing-in is toggled, show details or hide details
     $scope.toggleSignIn = function() {
       $scope.showSignIn = !$scope.showSignIn;
     };
-    
     // if advanced sing-up is toggled, show details or hide details
     $scope.toggleSignUp = function() {
       $scope.showSignUp = !$scope.showSignUp;
     };
-    
     // If user is signed in then redirect back home
     if ($scope.authentication.user) {
       $location.path('/');
@@ -48,17 +45,16 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$root
 
     $scope.signup = function (isValid) {
       $scope.error = null;
-
       if (!isValid) {
         $scope.$broadcast('show-errors-check-validity', 'userForm');
-
         return false;
       }
-
+      // set the current global language
+      $scope.credentials.languagePreference = LanguageService.globalCurrentLanguage;
+      // POST the creds to sign up
       $http.post('/api/auth/signup', $scope.credentials).success(function (response) {
         // If successful we assign the response to the global user model
         $scope.authentication.user = response;
-
         // And redirect to the previous or home page
         $state.go($state.previous.state.name || 'home', $state.previous.params);
       }).error(function (response) {
@@ -91,7 +87,6 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$root
       if ($state.previous && $state.previous.href) {
         url += '?redirect_to=' + encodeURIComponent($state.previous.href);
       }
-
       // Effectively call OAuth authentication route:
       $window.location.href = url;
     };
