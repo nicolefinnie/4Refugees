83b72fcf96da5d631a302dcf732e9f0a7f68c1c0
Nicole Finnie <Nicole.Finnie@de.ibm.com>
2016-02-12 01:15:03 +0100

    task 6606 use a tag array instead of hard coded list for

--- modules/users/client/controllers/settings/edit-profile.client.controller.js
+++ modules/users/client/controllers/settings/edit-profile.client.controller.js
@@ -73,20 +73,18 @@ angular.module('users').controller('EditProfileController', ['$scope', '$rootSco
     
     // Tags for skills and interests
     $scope.loadTags = function($query) {
-      var tags = [
-                  { tagID: '1', tagName: 'Job Training' },
-                  { tagID: '2', tagName: 'Language Courses' },
-                  { tagID: '3', tagName: 'Medical Assistance' },
-                  { tagID: '4', tagName: 'Childcare' },
-                  { tagID: '5', tagName: 'Others' },
-                  { tagID: '6', tagName: 'Food Preparation' },
-                  { tagID: '7', tagName: 'Customer Service' },
-                  { tagID: '8', tagName: 'Hair Cutting' },
-                  { tagID: '9', tagName: 'Plumbing' },
-                  { tagID: '10', tagName: 'Mechanics' },
-                  { tagID: '11', tagName: 'General Labour' },
-                  { tagID: '12', tagName: 'Construction' }
-      ];
+      var tagList = ['Job Training', 'Language Courses', 'Medical Assistance', 'Childcare', 'Others', 'Food Preparation', 
+                     'Customer Service', 'Hair Cutting', 'Plumbing', 'Mechanics', 'General Labour', 'Construction'];
+      
+      var tags = [];
+      for(var i = 0; i < tagList.length; i++){
+        var newTag = {
+          tagID: i+1,
+          tagName: tagList[i]
+        };                                                                     
+        tags.push(newTag);
+      }
+    
       return tags.filter(function(tag) {
         return tag.tagName.toLowerCase().indexOf($query.toLowerCase()) !== -1;
       });
